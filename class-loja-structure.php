59a33bead6018acc0c88d79f790f46521ef4dff8
Vitor H Rodrigues <vitorhugo.ro10@gmail.com>
2017-01-03 20:43:24 -0200

    Mudanças metaboxes

--- class-loja-structure.php
+++ class-loja-structure.php
@@ -154,27 +154,61 @@ class LojaWPPagSeguro
         )
     ) );
 
-    $data->add_group_field( $data_group_id, array(
-        'name' => 'Grupo',
-        'id'   => 'grupo',
-        'type' => 'select',
-        'show_option_none'  => false,
-        'default' => 'expo',
-        'options' => array(
-          'expo'  => 'Exposição',
-          'org'   => 'Organização',
-        )
-    ) );
 
+    $valores = new_cmb2_box(array(
+       'id'     => 'valores_metabox',
+       'title' => 'Valores dos ingressos',
+       'object_types'  => array('loja-cpt'),
+       'context'   => 'normal',
+       'priority'  => 'core',
+       'show_names'  => true
+    ));
+
+    $valores_group_id = $valores->add_field(array(
+	    'id'    => $this->prefix . 'valores',
+	    'type'  => 'group',
+	    'options' => array(
+		    'group_title' => 'Ingresso {#}',
+		    'add_button'    => 'Adicionar novo valor',
+		    'remove_button' => 'Remover valor',
+		    'sortable'      => true, // beta
+	    )
+    ));
+
+	  $valores->add_group_field( $valores_group_id, array(
+          'name' => 'Valor',
+          'id'   => 'valor',
+          'type' => 'text_money',
+          'before_field'    => 'R$'
+      ) );
+
+	  $valores->add_group_field( $valores_group_id, array(
+		  'name' => 'Vai ser para multiplos dias?',
+		  'id'   => 'multiplo',
+		  'type' => 'checkbox'
+      ) );
+
+	  $valores->add_group_field( $valores_group_id, array(
+		  'name' => 'Dias',
+		  'id'   => 'dias-multiplo',
+		  'type' => 'multicheck',
+		  'options' => array(
+			  'check1' => 'Check One',
+			  'check2' => 'Check Two',
+			  'check3' => 'Check Three',
+		  ),
+          'show_on'  => array($this, 'vhr_multiplo_check')
+	  ) );
+  }
+
+  public function vhr_multiplo_check(){
 
   }
 
   public function vhr_metaboxes(){
     /**
-     *  Dados do Evento (data, valores)
+     *  Dados do Evento (valores)
      */
-
-     add_meta_box( 'atributos', 'Atributos do Evento', array($this, 'vhr_screen_attributes') , 'loja-cpt', 'normal', 'default' );
 
     /**
      * Páginas relacionadas ao Evento
@@ -187,120 +221,5 @@ class LojaWPPagSeguro
      */
   }
 
-  public function vhr_screen_attributes($post){
-    ?>
-    <style media="screen">
-    .vhr-tabs {
-      color: #333;
-      display: block;
-      font-family: arial, sans-serif;
-      margin: auto;
-      position: relative;
-      width: 100%;
-    }
-
-      .vhr-tabs input[name="sections"] {
-        left: -9999px;
-        position: absolute;
-        top: -9999px;
-      }
-
-      .vhr-tabs section {
-        display: block;
-      }
-
-      .vhr-tabs section label {
-        background: #ccc;
-        border:1px solid #fff;
-        cursor: pointer;
-        display: block;
-        font-size: 1.2em;
-        font-weight: bold;
-        padding: 15px 20px;
-        position: relative;
-        width: 180px;
-        z-index:100;
-      }
-
-      .vhr-tabs section article {
-        display: none;
-        left: 230px;
-        min-width: 300px;
-        padding: 0 0 0 21px;
-        position: absolute;
-        top: 0;
-      }
-
-      .vhr-tabs section article:after {
-        background-color: #ccc;
-        bottom: 0;
-        content: "";
-        display: block;
-        left:-229px;
-        position: absolute;
-        top: 0;
-        width: 220px;
-        z-index:1;
-      }
-
-      .vhr-tabs input[name="sections"]:checked + label {
-        background: #eee;
-        color: #bbb;
-      }
-
-      .vhr-tabs input[name="sections"]:checked ~ article {
-        display: block;
-      }
-
-
-      @media (max-width: 533px) {
-
-      .vhr-tabs {
-        width: 100%;
-      }
-
-      .vhr-tabs section label {
-        font-size: 1em;
-        width: 160px;
-      }
-
-      .vhr-tabs section article {
-        left: 200px;
-        min-width: 270px;
-      }
-
-      .vhr-tabs section article:after {
-        background-color: #ccc;
-        bottom: 0;
-        content: "";
-        display: block;
-        left:-199px;
-        position: absolute;
-        top: 0;
-        width: 200px;
-
-      }
-
-      }
-
-    </style>
-      <div class="vhr-tabs">
-        <section id="section1">
-          <input type="radio" name="sections" id="data" checked>
-          <label for="data">Data</label>
-          <article>
-            Teste
-          </article>
-        </section>
-        <section id="section2">
-          <input type="radio" name="sections" id="valores">
-          <label for="valores">Valores</label>
-          <article>
-            Valores
-          </article>
-        </section>
-      </div>
-    <?php
-  }
 
 }
