ac32f3687b50882b85a0782aaba406c7572b7d2f
Vitor Hugo Rodrigues <vitorhugo.ro10@gmail.com>
2017-03-08 16:30:47 -0300

    ajuste estrutural

--- loja-wp-pagseguro.php
+++ loja-wp-pagseguro.php
@@ -9,13 +9,14 @@ Author URI: https://github.com/vitorhugoro1/
 */
 
 define('LOJA_ROOT', plugin_dir_path( __FILE__ ));
+define('LOJA_INCLUDES', LOJA_ROOT . 'includes/');
 define('LOJA_ASSETS', LOJA_ROOT . 'assets/');
 
-require LOJA_ROOT . 'pagseguro/vendor/autoload.php';
-require LOJA_ROOT . 'PHPExcel-1.8/vendor/autoload.php';
-require LOJA_ROOT . 'cmb2/init.php';
-require LOJA_ROOT . 'cmb2-conditionals/cmb2-conditionals.php';
-require LOJA_ROOT . 'cmb2-date-range/wds-cmb2-date-range-field.php';
+require LOJA_INCLUDES . 'pagseguro/vendor/autoload.php';
+require LOJA_INCLUDES . 'phpexcel/vendor/autoload.php';
+require LOJA_INCLUDES . 'cmb2/init.php';
+require LOJA_INCLUDES . 'cmb2-conditionals/cmb2-conditionals.php';
+require LOJA_INCLUDES . 'cmb2-date-range/wds-cmb2-date-range-field.php';
 require LOJA_ROOT . 'class-pagseguro-config.php';
 require LOJA_ROOT . 'class-pager-template.php';
 require LOJA_ROOT . 'class-post-types.php';
